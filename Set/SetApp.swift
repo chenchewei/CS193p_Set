7e673e7c3296af11749d52c900415c5721c12c76
chenchewei <gunattack1120@gmail.com>
2022-07-13 19:31:08 +0800

    feat: initializing project

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Anderson Chen on 2022/7/13.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
